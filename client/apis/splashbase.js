2e3cb6dee93692d55e45deb6eaf17a6954833ff4
student <student@eda-bc-07.localdomain>
2020-09-04 09:50:06 +1200

    getimage by search working

--- client/apis/splashbase.js
+++ client/apis/splashbase.js
@@ -1,17 +1,20 @@
 import request from 'superagent'
 
-const apiURL = 'http://www.splashbase.co/api/v1/'
+const apiURL = 'http://www.splashbase.co/api/v1/images'
+// http://www.splashbase.co/api/v1/images/1
+// http://www.splashbase.co/api/v1/images/search/?query=laptop
 
-export function getImages (query) {
+export function getImages(query) {
+  return request
+    .get(apiURL + "/search/?query=" + query)
+    .then(response => {
+      console.log(response.body)
+      return response.body
+    })
+}
+
+export function getRandomImage() {
   // return request
   //   .get(apiURL)
   //   .then(response => response.body)
 }
-
-export function getRandomImage () {
-  // return request
-  //   .get(apiURL)
-  //   .then(response => response.body)
-}
-
-// getImages("laptop")
